fa9e6dd22c877ce52bd64cb493381185bd669457
tuannvbg <nguyentuan2005@gmail.com>
2020-07-03 13:56:40 +0700

    Update demo.js

--- example/asset/demo.js
+++ example/asset/demo.js
@@ -15,7 +15,13 @@
             author: 'Jason Mraz',
             src: 'https://zhangchen2397.github.io/audioPlayer/example/asset/music/i_am_yours.mp4',
             cover: 'https://zhangchen2397.github.io/audioPlayer/example/asset/image/cover_iay.jpg?ver=1'
-        }]
+        }, {
+            title: 'Mỹ Nhân',
+            author: 'Đinh Đại Vũ',
+            src: 'https://archive.org/download/dinh.-dai.-vu-my.-nhan/Dinh.Dai.Vu-My.Nhan.mp3',
+            cover: 'https://avatar-nct.nixcdn.com/singer/avatar/2018/03/27/3/f/5/f/1522142059144.jpg'
+        }
+              ]
     });
 
     $.bind(audioIns, 'play', function(event) {
